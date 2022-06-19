9d345a33007f2eddfaae9c7ae1a77b67c2766594
RageLeague <34314733+RageLeague@users.noreply.github.com>
2022-06-18 21:54:54 -0600

    Update SaveCurrentProfile.lua

--- api/SaveCurrentProfile.lua
+++ api/SaveCurrentProfile.lua
@@ -1,4 +1,5 @@
-function Containers.SaveCurrentProfile(id, forced_save)
+function Containers.SaveCurrentProfile(id, forced_save, data)
+    data = data or {}
     if not id then
         id = engine.inst:NewGUID()
         print(string.format("No ID provided, generate new one (%s)", id))
@@ -9,16 +10,34 @@ function Containers.SaveCurrentProfile(id, forced_save)
         exists = result
     end
     Containers.settings:FileExists( string.format("%s/%s/info.lua", Containers.folder, id), OnFileExists )
+    local info = {}
     if exists and not forced_save then
         print("Container already exists. Halt function.")
         return false
     end
+    if exists then
+        local ok, result = xpcall( require, generic_error, string.format("%s%s/%s/info.lua", Container.path, Containers.folder, id) )
+        if not ok then
+            print(result)
+            info = {
+                name = data.name or "Container",
+            }
+        else
+            info = result
+        end
+    else
+        info = {
+            name = data.name or "Container",
+        }
+    end
+    if not info.time_created then
+        info.time_created = os.time()
+    end
+
     print(string.format("Saving to save (%s)...", id))
     Containers.CopyProfile("", Containers.folder .. "/" .. id .. "/")
     local f = io.open(Containers.path .. Containers.folder .. "/" .. id .. "/info.lua", "w")
-    local info = {
-        name = "Container",
-    }
+    info.last_saved = os.time()
     local out = "return ".. serpent.block(info, {comment = false})
     f:write(out)
     f:close()

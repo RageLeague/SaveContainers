9959a5f80627b2256db3bede69fa2217ffdd23d8
RageLeague <34314733+RageLeague@users.noreply.github.com>
2022-06-18 21:15:00 -0600

    Update ListProfiles.lua

--- api/ListProfiles.lua
+++ api/ListProfiles.lua
@@ -4,9 +4,16 @@ function Containers.ListProfiles()
     local paths = {}
     local constructed_path = string.format('%s%s/', Containers.path, Containers.folder)
     for k, filename in ipairs( filepath.list_files( constructed_path, "info.lua", true )) do
-        local name = filename:match( "([^/]+)/info[.]lua$" )
+        local id = filename:match( "([^/]+)/info[.]lua$" )
+        local name = filename:match( "(.+)[.]lua$" )
+        local ok, result = xpcall( require, generic_error, name )
         print(name)
-        table.insert(paths, name)
+        -- table.insert(paths, name)
+        if ok then
+            paths[id] = result
+        else
+            print(result)
+        end
     end
     return paths
 end

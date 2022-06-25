16e20984628f5f4d290afa50a6feea43e1b11c6f
RageLeague <34314733+RageLeague@users.noreply.github.com>
2022-06-25 16:53:30 -0600

    Add steam desc

--- modinit.lua
+++ modinit.lua
@@ -15,8 +15,23 @@ local function OnLoad( mod )
 
 end
 
+local function OnPreLoad()
+    for k, filepath in ipairs( filepath.list_files( "SAVE_CONTAINERS:loc", "*.po", true )) do
+        local name = filepath:match( "(.+)[.]po$" )
+        local lang_id = name:match("([_%w]+)$")
+        lang_id = lang_id:gsub("_", "-")
+        -- require(name)
+        print(lang_id)
+        for id, data in pairs(Content.GetLocalizations()) do
+            if data.default_languages and table.arraycontains(data.default_languages, lang_id) then
+                Content.AddPOFileToLocalization(id, filepath)
+            end
+        end
+    end
+end
+
 return {
-    version = "0.0.1",
+    version = "0.1.0",
     alias = "SAVE_CONTAINERS",
 
     OnPreLoad = OnPreLoad,
